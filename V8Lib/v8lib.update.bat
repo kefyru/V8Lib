23e6b3d04120fd55aefe2b3b393e81a21faa50bb
kefy <kefy@mail.ru>
2021-03-18 17:15:19 +0300

    1

--- V8Lib/v8lib.update.bat
+++ V8Lib/v8lib.update.bat
@@ -1,3 +1,3 @@
 @echo off
-curl "https://raw.githubusercontent.com/kefyru/V8Lib/master/V8Lib/v8lib.register.bat" --output "%ProgramFiles\1cv8\v8lib\v8lib.register.bat"
-"%ProgramFiles\1cv8\v8lib\v8lib.register.bat"
+curl "https://raw.githubusercontent.com/kefyru/V8Lib/master/V8Lib/v8lib.register.bat" --output "%ProgramFiles%\1cv8\v8lib\v8lib.register.bat"
+"%ProgramFiles%\1cv8\v8lib\v8lib.register.bat"
