7257e66c1ed3ac7a417c2d7ad4aad5ef09ae145f
kefy <kefy@mail.ru>
2021-03-18 16:29:14 +0300

    1

--- V8Lib/v8lib.register.bat
+++ V8Lib/v8lib.register.bat
@@ -1,5 +1,5 @@
 @echo off
-curl https://github.com/kefyru/V8Lib/blob/master/V8Lib/v8lib.update.bat --output %ProgramFiles\1cv8\v8lib\v8lib.update.bat
-curl https://github.com/kefyru/V8Lib/blob/master/V8Lib/bin/Debug/V8Lib.dll --output %ProgramFiles\1cv8\v8lib\V8Lib.dll
-curl https://github.com/kefyru/V8Lib/blob/master/V8Lib/bin/Debug/V8Lib.pdb --output %ProgramFiles\1cv8\v8lib\V8Lib.pdb
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\regasm.exe %ProgramFiles\1cv8\v8lib\V8Lib.dll /tlb /codebase /verbose
+curl https://github.com/kefyru/V8Lib/master/V8Lib/v8lib.update.bat?raw=true --create-dirs --output %ProgramFiles\1cv8\v8lib\v8lib.update.bat
+curl https://github.com/kefyru/V8Lib/blob/master/V8Lib/bin/Debug/V8Lib.dll?raw=true --create-dirs --output %ProgramFiles\1cv8\v8lib\V8Lib.dll
+curl https://github.com/kefyru/V8Lib/blob/master/V8Lib/bin/Debug/V8Lib.pdb?raw=true --create-dirs --output %ProgramFiles\1cv8\v8lib\V8Lib.pdb
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\regasm.exe %ProgramFiles\1cv8\v8lib\V8Lib.dll /tlb /codebase /verbose
